b7a9caa4fd20d17a96a6c782b9d2fd0804ec5583
Mark Lewis <mlewis89@gmail.com>
2024-04-03 10:07:46 +1100

    Product tag model

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,26 +6,7 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'product',
-        key: 'id'
-      }
-    },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'tag',
-        key: 'id'
-      }
-    }
+    
 
   },
   {
