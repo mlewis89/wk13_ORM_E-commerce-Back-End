9da8f00df68db43485fb041ee3bcff77b47ad701
Mark Lewis <mlewis89@gmail.com>
2024-04-04 17:45:33 +1100

    get by id routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -7,7 +7,8 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData =  await Category.findAll();
+    const categoryData =  await Category.findAll({
+      include: [{model: Product}]});
     res.status(200).json(categoryData);
     
   } catch (error) {
@@ -16,9 +17,23 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData =  await Category.findByPk(req.params.id,{
+      include: [{model: Product}]
+    });
+    if(!categoryData)
+    {
+      res.status(404).json({message: 'no category found'});
+      return;
+    }
+    res.status(200).json(categoryData);
+    
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post('/', (req, res) => {
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,16 +7,32 @@ router.get('/', async(req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.findAll({
+      include: [{model:Product}]
+    });
     res.status(200).json(tagData);
   } catch (error) {
     res.status(500).json(error);    
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id,{
+      include: [{model:Product}]
+    });
+    if(!tagData)
+    {
+      res.status(404).json({message: 'no category found'});
+      return;
+    }
+    
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);    
+  }
 });
 
 router.post('/', (req, res) => {
