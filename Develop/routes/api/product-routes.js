64cb03456bb749c110fbc3dd995a7b8432f4b3f1
Mark Lewis <mlewis89@gmail.com>
2024-04-04 16:19:06 +1100

    get all reoutes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,9 +4,15 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData =  await Product.findAll();
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
