5cc1d86bd973d25b85c08a45ad6d880894f74c81
Mark Lewis <mlewis89@gmail.com>
2024-04-09 08:29:55 +1000

    changed price to 2 decimal points

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -23,7 +23,7 @@ Product.init(
     },
     //Product Price
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
         isDecimal: true
